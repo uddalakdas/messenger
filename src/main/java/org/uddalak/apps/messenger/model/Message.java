82e75f7133baa4f98849b7dd07aa1b16112d5cf9
Uddalak Das <uddalakdas@yahoo.com>
2016-03-24 12:59:00 +0530

    First commit

---
+++ src/main/java/org/uddalak/apps/messenger/model/Message.java
@@ -0,0 +1,11 @@
+package org.uddalak.apps.messenger.model;
+
+import java.util.Date;
+
+public class Message {
+	
+	public long id;
+	private String message;
+	private Date created;
+	private String author;
+}
